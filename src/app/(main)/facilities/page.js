6f8b3c16210fadcb7debc390d5f01a5a91c6e8d2
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-07-18 09:51:50 +0600

    feat: finalized facilities page design

--- src/app/(main)/facilities/page.js
+++ src/app/(main)/facilities/page.js
@@ -1,7 +1,16 @@
+'use client';
+
+import PageHeading from '@/components/ui/PageHeading';
+import FacilitiesSection from '@/components/modules/facilities/FacilitiesSection';
+
 export default function FacilitiesPage() {
     return (
-        <div>
-            <h1>facilities page</h1>
+        <div className="bg-background min-h-screen w-full px-5 md:px-6 lg:px-8">
+            <PageHeading
+                title={'Browse Facilities'}
+                paragraph={'Discover top-rated venues for every game.'}
+            />
+            <FacilitiesSection />
         </div>
     );
 }
